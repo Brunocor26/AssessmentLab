e05af1215579875f9ee33f3490c3d98e14d6f134
Brunocor26 <152163216+Brunocor26@users.noreply.github.com>
2025-04-14 11:00:12 +0100

    Update index.js

--- AL_NODE_1/index.js
+++ AL_NODE_1/index.js
@@ -8,35 +8,35 @@ let minhas_notas = [20, 10, 15, 17];
 
 // a. devolve todas as notas
 app.get('/', (req, res) => {
-  res.status(200).json(minhas_notas);
+  res.status(200).json(minhas_notas);  //status 200-> bem sucedido
 });
 
 // b. devolve a nota na posição (parametro)
 app.get('/:posicao', (req, res) => {
-  const pos = parseInt(req.params.posicao);
+  const pos = parseInt(req.params.posicao); //vai buscar a posiçao ao request e guarda no pos
   if (isNaN(pos) || pos < 0 || pos >= minhas_notas.length) {
-    return res.status(400).json({ erro: 'Posição inválida' });
+    return res.status(400).json({ erro: 'Posição inválida' }); //status 400-> erro se a posicao nao é valida (maior do que o tamanho ou nao é um numero)
   }
-  res.status(200).json(minhas_notas[pos]);
+  res.status(200).json(minhas_notas[pos]); //da a resposta como json (da nota na posicao pedida)
 });
 
 // c. adiciona uma nota do body
 app.post('/', (req, res) => {
-  let nota = parseInt(req.body.nota);
-  if (isNaN(nota)) {
+  let nota = parseInt(req.body.nota);  //tenta ir buscar a nota ao corpo da request e converte para int | usamos o body porque é passado por json
+  if (isNaN(nota)) { //se nao é numero
     return res.status(400).json({ erro: 'Nota inválida' });
   }
   minhas_notas.push(nota);
   res.status(200).json({ mensagem: 'Nota adicionada com sucesso', notas: minhas_notas });
 });
 
-// d.adiciona uma nota mas passada na URL
-app.post('/adicionar/:nota', (req, res) => {
-  let nota = parseInt(req.params.nota);
+// d.adiciona uma nota mas passada pelo URL
+app.post('/adicionar/:nota', (req, res) => {  //:nota é parametro de rota
+  let nota = parseInt(req.params.nota); //req.params porque o que queremos ir buscar faz parte do caminho
   if (isNaN(nota)) {
     return res.status(400).json({ erro: 'Nota inválida!' });
   }
-  minhas_notas.push(nota);
+  minhas_notas.push(nota); //push na lsita
   res.status(200).json({ mensagem: 'Nota adicionada com sucesso através de parâmetro', notas: minhas_notas });
 });
 
@@ -61,30 +61,17 @@ app.delete('/apagar/:posicao', (req, res) => {
     return res.status(400).json({ erro: 'Posição inválida!' });
   }
 
-  minhas_notas.splice(pos, 1);
+  minhas_notas.splice(pos, 1); //splice remove n elementos(neste caso 1) a partir da posicao pos
   res.status(200).json({ mensagem: 'Nota apagada com sucesso', notas: minhas_notas });
 });
 
 // g. apaga tudo
 app.delete('/', (req, res) => {
-  minhas_notas = [];
+  minhas_notas = []; //passa a ser vazio
   res.status(200).json({ mensagem: 'Todas as notas foram apagadas' });
 });
 
 // inicalizar sv
 app.listen(port, () => {
-  console.log('Notas- feito por Bruno Correia e Alexandre Santos at http://localhost:' + port);
+  console.log('Notas- feito por Bruno Correia e Alexandre Santos em http://localhost:' + port);
 });
-
-
-
-/* TESTAR na extensão
-
-Método	URL	                                  Body	           Ação
-GET	    http://localhost:3000/          		                Mostra todas as notas
-GET	    http://localhost:3000/2		                          Mostra nota da posição 2
-POST  	http://localhost:3000/	        { "nota": 18 }      Adiciona nota pelo body
-POST	  http://localhost:3000/adicionar/19	    	          Adiciona nota pelo URL
-PATCH	  http://localhost:3000/atualizar/1	  { "nota": 20 }	Atualiza nota da posição 1
-DELETE	http://localhost:3000/apagar/0	                  	Apaga a nota na posição 0
-DELETE	http://localhost:3000/		                          Apaga todas as notas */
